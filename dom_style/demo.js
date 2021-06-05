1c0aecbd32491ffb4c7ea2f82de6f25cc498f42f
alamgirweb11 <alamgirweb11@gmail.com>
2021-06-05 23:39:46 +0600

    modify dom style

--- dom_style/demo.js
+++ dom_style/demo.js
@@ -1,3 +1,10 @@
 let title = document.getElementById('title');
 title.style.color = 'red';
+let domStyle = {
+        background: 'gray',
+        fontSize: '25px',
+        color: 'white'
+};
 let list = document.getElementById('list');
+[...list.children].forEach(li => Object.assign(li.style, domStyle));
+
